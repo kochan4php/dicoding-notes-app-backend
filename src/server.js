887b7e1d35501d99841b20b6970fd0ba8ff76223
kochan4php <aprodeosubarno@gmail.com>
2023-05-07 15:19:42 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import Hapi from '@hapi/hapi'
+import api from './routes/api.js'
+import hapiCors from 'hapi-cors'
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: true
+        }
+    })
+    await server.register(hapiCors)
+
+    server.route(api)
+
+    await server.start()
+    console.log('Server is running on %s', server.info.uri)
+}
+
+export default init
