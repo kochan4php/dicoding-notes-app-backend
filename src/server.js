012c0a67020ffd637bdb90f89a11a954b0c2be09
kochan4php <aprodeosubarno@gmail.com>
2023-05-07 15:30:57 +0700

    fix bug, maybe

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import hapiCors from 'hapi-cors'
 const init = async () => {
     const server = Hapi.server({
         port: 3000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        // host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: true
         }
